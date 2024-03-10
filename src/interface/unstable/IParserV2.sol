b380a70698a7a1e9f0249f3d38d20f9b6c16b276
thedavidmeister <thedavidmeister@gmail.com>
2024-03-10 21:20:37 +0400

    view parser

--- src/interface/unstable/IParserV2.sol
+++ src/interface/unstable/IParserV2.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.18;
 
 interface IParserV2 {
-    function parse2(bytes calldata data) external pure returns (bytes calldata bytecode);
+    function parse2(bytes calldata data) external view returns (bytes calldata bytecode);
 }
