2d8d3a810def2d1a9e098dbdf5c7ec16e6b6fe57
thedavidmeister <thedavidmeister@gmail.com>
2024-03-25 22:46:51 +0400

    remove dupes

--- src/interface/unstable/IInterpreterCallerV3.sol
+++ src/interface/unstable/IInterpreterCallerV3.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.18;
 import {IParserV2} from "./IParserV2.sol";
 import {IInterpreterStoreV2} from "../IInterpreterStoreV2.sol";
 import {IInterpreterV3} from "./IInterpreterV3.sol";
+import {
+    SignedContextV1,
+    SIGNED_CONTEXT_SIGNER_OFFSET,
+    SIGNED_CONTEXT_CONTEXT_OFFSET,
+    SIGNED_CONTEXT_SIGNATURE_OFFSET
+} from "../IInterpreterCallerV2.sol";
 
 /// Struct over the return of `IParserV2.parse2` which MAY be more convenient to
 /// work with than raw addresses.
@@ -15,47 +21,6 @@ struct EvaluableV3 {
     IInterpreterStoreV2 store;
     bytes bytecode;
 }
-
-/// Typed embodiment of some context data with associated signer and signature.
-/// The signature MUST be over the packed encoded bytes of the context array,
-/// i.e. the context array concatenated as bytes without the length prefix, then
-/// hashed, then handled as per EIP-191 to produce a final hash to be signed.
-///
-/// The calling contract (likely with the help of `LibContext`) is responsible
-/// for ensuring the authenticity of the signature, but not authorizing _who_ can
-/// sign. IN ADDITION to authorisation of the signer to known-good entities the
-/// expression is also responsible for:
-///
-/// - Enforcing the context is the expected data (e.g. with a domain separator)
-/// - Tracking and enforcing nonces if signed contexts are only usable one time
-/// - Tracking and enforcing uniqueness of signed data if relevant
-/// - Checking and enforcing expiry times if present and relevant in the context
-/// - Many other potential constraints that expressions may want to enforce
-///
-/// EIP-1271 smart contract signatures are supported in addition to EOA
-/// signatures via. the Open Zeppelin `SignatureChecker` library, which is
-/// wrapped by `LibContext.build`. As smart contract signatures are checked
-/// onchain they CAN BE REVOKED AT ANY MOMENT as the smart contract can simply
-/// return `false` when it previously returned `true`.
-///
-/// @param signer The account that produced the signature for `context`. The
-/// calling contract MUST authenticate that the signer produced the signature.
-/// @param context The signed data in a format that can be merged into a
-/// 2-dimensional context matrix as-is.
-/// @param signature The cryptographic signature for `context`. The calling
-/// contract MUST authenticate that the signature is valid for the `signer` and
-/// `context`.
-struct SignedContextV1 {
-    // The ordering of these fields is important and used in assembly offset
-    // calculations and hashing.
-    address signer;
-    uint256[] context;
-    bytes signature;
-}
-
-uint256 constant SIGNED_CONTEXT_SIGNER_OFFSET = 0;
-uint256 constant SIGNED_CONTEXT_CONTEXT_OFFSET = 0x20;
-uint256 constant SIGNED_CONTEXT_SIGNATURE_OFFSET = 0x40;
 
 /// @title IInterpreterCallerV3
 /// @notice A contract that calls an `IInterpreterV3` via. `eval3`. There are
