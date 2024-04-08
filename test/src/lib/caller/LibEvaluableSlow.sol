96e3a58f967aece155c006c651f79879f3ed8f6f
thedavidmeister <thedavidmeister@gmail.com>
2024-04-08 23:20:48 +0400

    hashing evaluable v3

--- test/src/lib/caller/LibEvaluableSlow.sol
+++ test/src/lib/caller/LibEvaluableSlow.sol
@@ -2,6 +2,7 @@
 pragma solidity =0.8.19;
 
 import {EvaluableV2} from "src/interface/IInterpreterCallerV2.sol";
+import {EvaluableV3} from "src/interface/unstable/IInterpreterCallerV3.sol";
 
 library LibEvaluableSlow {
     function hashSlow(EvaluableV2 memory evaluable) internal pure returns (bytes32) {
@@ -10,6 +11,19 @@ library LibEvaluableSlow {
                 uint256(uint160(address(evaluable.interpreter))),
                 uint256(uint160(address(evaluable.store))),
                 uint256(uint160(evaluable.expression))
+            )
+        );
+    }
+
+    function hashSlow(EvaluableV3 memory evaluable) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                keccak256(
+                    abi.encodePacked(
+                        uint256(uint160(address(evaluable.interpreter))), uint256(uint160(address(evaluable.store)))
+                    )
+                ),
+                keccak256(evaluable.bytecode)
             )
         );
     }
