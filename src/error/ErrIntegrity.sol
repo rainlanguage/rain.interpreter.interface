fde57120ace784d0431738b3c332db5e54cac27b
thedavidmeister <thedavidmeister@gmail.com>
2024-10-20 19:07:15 +0400

    wip on optest

---
+++ src/error/ErrIntegrity.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity ^0.8.25;
+
+/// The bytecode and integrity function disagree on number of inputs.
+/// @param opIndex The index of the operation in question.
+/// @param calculatedInputs The number of inputs calculated by the integrity function.
+/// @param bytecodeInputs The number of inputs in the bytecode.
+error BadOpInputsLength(uint256 opIndex, uint256 calculatedInputs, uint256 bytecodeInputs);
+
+/// The bytecode and integrity function disagree on number of outputs.
+/// @param opIndex The index of the operation in question.
+/// @param calculatedOutputs The number of outputs calculated by the integrity function.
+/// @param bytecodeOutputs The number of outputs in the bytecode.
+error BadOpOutputsLength(uint256 opIndex, uint256 calculatedOutputs, uint256 bytecodeOutputs);
